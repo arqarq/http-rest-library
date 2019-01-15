37e88dfdd25f0b4b2a616d21410576912f86d6b0
arqarq <43341071+arqarq@users.noreply.github.com>
2019-01-16 00:30:22 +0100

    - get all z własnym nagłówkiem

--- src/main/java/pl/sda/httprestlibrary/controller/LibControl.java
+++ src/main/java/pl/sda/httprestlibrary/controller/LibControl.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sda.httprestlibrary.model.Book;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,7 +15,8 @@ public class LibControl {
     private List<Book> bookList = new LinkedList<>();
 
     @GetMapping
-    ResponseEntity<?> getAllBooks() {
-        return new ResponseEntity<>(bookList, HttpStatus.OK);
+    ResponseEntity<?> getAllBooks(HttpServletResponse response) {
+        response.addHeader("Arek", "Sekuła");
+        return new ResponseEntity<>(bookList, HttpStatus.I_AM_A_TEAPOT);
     }
 }
